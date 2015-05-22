092b164e876cb1e4dda0aa605262f0e2760f99b3
Ytrog <hisbrucker@gmail.com>
2015-05-22 23:29:55 +0200

    Project created

---
+++ AreaBudgetCalculator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AreaBudgetCalculator.Startup))]
+namespace AreaBudgetCalculator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
